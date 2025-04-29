ad4ef9a16b3e82e6e01c59337ba5ffe796b43200
liam <iliamftw2013@gmail.com>
2025-04-29 15:53:08 -0300

    Add: ResourceLayer for User

---
+++ src/main/java/com/sode/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.sode.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sode.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Liam", "liam@email.com", "123213213", "xxxxxxxxxxxxx");
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
+
