ece43636215c42a1b4fe1ec6f5958015eae7df68
liam <iliamftw2013@gmail.com>
2025-05-01 16:03:23 -0300

    Add: usr updates

--- src/main/java/com/sode/course/services/UserService.java
+++ src/main/java/com/sode/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.sode.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sode.course.entities.User;
@@ -31,5 +32,19 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User usr) {
+		User entityUser = repository.getReferenceById(id);
+		updateData(entityUser, usr);
+		return repository.save(entityUser);
+		
+		
+	}
+
+	private void updateData(User entityUser, User usr) {
+		entityUser.setName(usr.getName());
+		entityUser.setEmail(usr.getEmail());
+		entityUser.setPhone(usr.getPhone());
 	}
 }
